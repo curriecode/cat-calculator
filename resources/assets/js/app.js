5f41582d107bf4ed748d793bcd5f0ee3118ca93b
curriecode <melaniecurrie01@gmail.com>
2020-02-15 23:21:26 -0800

    add: registered catpic component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,8 +2,11 @@ require("./bootstrap");
 import Vue from "vue";
 import Calculator from "./components/Calculator";
 import App from "./components/App";
+import Catpic from "./components/Catpic.vue";
 
 Vue.component("Calculator", Calculator);
+Vue.component("Catpic", Catpic);
+
 const app = new Vue({
     el: "#app",
     render: h => h(App)
