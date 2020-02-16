1f75707a3c00322ba78514ad6e4a427f5c270769
curriecode <melaniecurrie01@gmail.com>
2020-02-16 13:27:28 -0800

    setup: header template with method to grab pic from api

---
+++ resources/assets/js/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="header" class="header">
+        <img class="cat" src="{{pic}}" />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            pic: null
+        };
+    },
+
+    created: {
+        headerPic() {
+            axios
+                .get("/api/cats/create")
+                .then(res => {
+                    this.pic = res.data[0].url;
+                })
+                .catch(err => {
+                    console.log("ERROR: ", err);
+                });
+        }
+    }
+};
+</script>
+<style scoped>
+.header {
+    display: flex;
+    width: 100%;
+    height: 20%;
+}
+</style>
