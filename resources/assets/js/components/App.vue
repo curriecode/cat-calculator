48098e791acb53326a9f27c58d2b1d83e4ab0554
curriecode <melaniecurrie01@gmail.com>
2020-02-16 14:03:14 -0800

    changed: order that components are rendered so header is at the top

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container">
+        <Header />
         <Calculator @equalClicked="req" @clearCat="clearCat" />
         <Catpic :catpic="catpic" />
-        <Header />
     </div>
 </template>
 <script>
